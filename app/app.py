e2f0eac2391b011f91dc285460f2986777e61353
georgehampson-b6 <georgeh@bolt6.ai>
2024-07-12 14:45:23 +0100

    Updates

--- app/app.py
+++ app/app.py
@@ -10,4 +10,5 @@ def home():
     return render_template('index.html', version=version, build_date=build_date)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
